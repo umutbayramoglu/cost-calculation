a0a27a5cd2347abf2247302333278278b0bb87cc
Umut Emre Bayramoğlu <umut265@gmail.com>
2022-05-04 22:11:35 +0300

    Create entity models
    Signed-off-by: Umut Emre Bayramoğlu <umut265@gmail.com>

---
+++ app/src/main/java/com/ueb/cost_calculation/cost_calculator/model/entity/ItemEntityModel.kt
@@ -0,0 +1,19 @@
+package com.ueb.cost_calculation.cost_calculator.model.entity
+
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.ueb.cost_calculation.base.model.entity.BaseEntityModel
+
+@Entity(tableName = "item")
+data class ItemEntityModel(
+
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
+
+    val name: String,
+    val count: Int,
+
+    @Embedded(prefix = "items_")
+    val items: List<ItemPartEntryEntityModel>
+) : BaseEntityModel()
---
+++ app/src/main/java/com/ueb/cost_calculation/cost_calculator/model/entity/ItemPartEntryEntityModel.kt
@@ -0,0 +1,9 @@
+package com.ueb.cost_calculation.cost_calculator.model.entity
+
+
+
+data class ItemPartEntryEntityModel(
+    val name: String,
+    val count: Int,
+    val price: Int
+)
