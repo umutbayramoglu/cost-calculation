e9f22083253ff6414a72dfad3903c42bfa58ef46
Umut Emre Bayramoğlu <umut265@gmail.com>
2022-05-05 09:28:02 +0300

    Implement cost calculator function
    Signed-off-by: Umut Emre Bayramoğlu <umut265@gmail.com>

--- app/src/main/java/com/ueb/cost_calculation/cost_calculator/ui/fragment/CostCalculatorFragment.kt
+++ app/src/main/java/com/ueb/cost_calculation/cost_calculator/ui/fragment/CostCalculatorFragment.kt
@@ -2,12 +2,14 @@ package com.ueb.cost_calculation.cost_calculator.ui.fragment
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import com.ueb.cost_calculation.R
 import com.ueb.cost_calculation.base.ui.fragment.BasePageFragment
+import com.ueb.cost_calculation.cost_calculator.model.entity.ItemEntityModel
 import com.ueb.cost_calculation.cost_calculator.ui.CostCalculatorViewModelFactory
 import kotlinx.android.synthetic.main.cost_calculator_fragment.*
 import kotlinx.coroutines.Dispatchers
@@ -24,6 +26,7 @@ class CostCalculatorFragment : BasePageFragment() {
     private val viewModelFactory by inject<CostCalculatorViewModelFactory>()
 
     private lateinit var viewModel: CostCalculatorViewModel
+    private var itemCostHashMap : MutableMap<String,Int> = HashMap()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,16 +40,38 @@ class CostCalculatorFragment : BasePageFragment() {
 
         viewModel =
             ViewModelProvider(this, viewModelFactory).get(CostCalculatorViewModel::class.java)
+
         initViews()
     }
 
     private fun initViews() = GlobalScope.launch(Dispatchers.Main) {
-        viewModel.items.await().observe(viewLifecycleOwner, Observer { itemList ->
-            if (itemList != null && itemList.isNotEmpty()) {
-                itemName.text = itemList[0].name
-                itemCount.text = itemList[0].count.toString()
-            }
 
+        viewModel.item.await().observe(viewLifecycleOwner, Observer { item ->
+            val cost = findItemCost(item)
+            tv_costView.text = cost.toString()
+            Log.e("CostCalculatorFragment", cost.toString())
         })
+    }
+
+    private fun findItemCost(item: ItemEntityModel?): Int{
+        if(item == null)
+            return 0;
+
+        if(itemCostHashMap.containsKey(item.name))
+            return itemCostHashMap[item.name]!!
+
+        // Check cur item is has sub-items or not
+        if(item.items == null){
+            return item.count * item.price!!
+        }
+
+        var curCost = 0
+        for(subItem in item.items){
+            curCost += findItemCost(subItem)
+        }
+        curCost *= item.count
+
+        itemCostHashMap[item.name] = curCost
+        return  curCost
     }
 }
