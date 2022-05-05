7ed3da5c41051550409702c91afdc05ead82ce1b
Umut Emre Bayramoğlu <umut265@gmail.com>
2022-05-05 05:00:00 +0300

    Show items on ui
    Signed-off-by: Umut Emre Bayramoğlu <umut265@gmail.com>

--- app/src/main/java/com/ueb/cost_calculation/cost_calculator/ui/fragment/CostCalculatorFragment.kt
+++ app/src/main/java/com/ueb/cost_calculation/cost_calculator/ui/fragment/CostCalculatorFragment.kt
@@ -5,9 +5,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import com.ueb.cost_calculation.R
 import com.ueb.cost_calculation.base.ui.fragment.BasePageFragment
 import com.ueb.cost_calculation.cost_calculator.ui.CostCalculatorViewModelFactory
+import kotlinx.android.synthetic.main.cost_calculator_fragment.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 
 class CostCalculatorFragment : BasePageFragment() {
@@ -30,9 +35,18 @@ class CostCalculatorFragment : BasePageFragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        viewModel = ViewModelProvider(this, viewModelFactory).get(CostCalculatorViewModel::class.java)
-
-
+        viewModel =
+            ViewModelProvider(this, viewModelFactory).get(CostCalculatorViewModel::class.java)
+        initViews()
     }
 
+    private fun initViews() = GlobalScope.launch(Dispatchers.Main) {
+        viewModel.items.await().observe(viewLifecycleOwner, Observer { itemList ->
+            if (itemList != null && itemList.isNotEmpty()) {
+                itemName.text = itemList[0].name
+                itemCount.text = itemList[0].count.toString()
+            }
+
+        })
+    }
 }
